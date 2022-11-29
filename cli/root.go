5e82f6b082454a7047a5ae1ae312a6ed89d688e0
divolgin <dmitriy@replicated.com>
2022-11-29 14:15:08 -0800

    Support for tgz files

--- cli/root.go
+++ cli/root.go
@@ -1,10 +1,15 @@
 package cli
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"fmt"
+	"io"
 	"io/fs"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/divolgin/helmsplain/pkg/values"
 	"github.com/pkg/errors"
@@ -62,9 +67,18 @@ func processArg(arg string) error {
 		if err != nil {
 			return errors.Wrapf(err, "walk dir %s", arg)
 		}
-	} else {
-		printValuesInFile(arg)
+		return nil
 	}
+
+	if isTGZ(arg) {
+		err := lookInTGZ(arg)
+		if err != nil {
+			return errors.Wrapf(err, "look in tgz %s", arg)
+		}
+		return nil
+	}
+
+	printValuesInFile(arg, "")
 
 	return nil
 }
@@ -79,7 +93,7 @@ func walkDir(root string) error {
 			return nil
 		}
 
-		printValuesInFile(path)
+		printValuesInFile(path, root)
 
 		return nil
 	})
@@ -87,7 +101,7 @@ func walkDir(root string) error {
 	return errors.Wrap(err, "walk dir")
 }
 
-func printValuesInFile(filename string) {
+func printValuesInFile(filename string, filenameMask string) {
 	refs, err := values.GetFromFiles(filename)
 	if err != nil {
 		fmt.Printf("%s\n", filename)
@@ -99,8 +113,93 @@ func printValuesInFile(filename string) {
 		return
 	}
 
-	fmt.Printf("%s\n", filename)
+	fmt.Printf("%s\n", strings.TrimPrefix(filename, filenameMask))
 	for _, ref := range refs {
 		fmt.Println("    ", ref)
 	}
+}
+
+func isTGZ(filename string) bool {
+	f, err := os.Open(filename)
+	if err != nil {
+		return false
+	}
+	defer f.Close()
+
+	gr, err := gzip.NewReader(f)
+	if err != nil {
+		return false
+	}
+	defer gr.Close()
+
+	return true
+}
+
+func lookInTGZ(archiveName string) error {
+	tmpDir, err := ioutil.TempDir("", "helmsplain-")
+	if err != nil {
+		return errors.Wrap(err, "create temp dir")
+	}
+	os.RemoveAll(tmpDir)
+
+	fileReader, err := os.Open(archiveName)
+	if err != nil {
+		return errors.Wrap(err, "open archive")
+	}
+	defer fileReader.Close()
+
+	gzipReader, err := gzip.NewReader(fileReader)
+	if err != nil {
+		return errors.Wrap(err, "new gzip reader")
+	}
+	defer gzipReader.Close()
+
+	tarReader := tar.NewReader(gzipReader)
+	for {
+		header, err := tarReader.Next()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return errors.Wrap(err, "read tar header")
+		}
+
+		if header.Typeflag != tar.TypeReg {
+			continue
+		}
+
+		err = func() error {
+			outFilename := filepath.Join(tmpDir, header.Name)
+			outPath := filepath.Dir(outFilename)
+			err = os.MkdirAll(outPath, 0755)
+			if err != nil {
+				return errors.Wrap(err, "create file path")
+			}
+
+			outFile, err := os.Create(outFilename)
+			if err != nil {
+				return errors.Wrap(err, "create output file")
+			}
+			defer outFile.Close()
+
+			_, err = io.Copy(outFile, tarReader)
+			if err != nil {
+				return errors.Wrapf(err, "copy file %s", header.Name)
+			}
+
+			return nil
+		}()
+
+		if err != nil {
+			return err
+		}
+	}
+
+	if err := walkDir(tmpDir); err != nil {
+		return errors.Wrapf(err, "walk temp dir")
+	}
+
+	return nil
 }
